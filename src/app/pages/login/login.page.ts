b1a1ca9639dc267cf66b435056b8c3bd3ff15696
MDZ2999 <mundoalexis1010@gmail.com>
2025-02-28 14:04:39 -0600

    V1.0

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonCol, IonTitle, IonToolbar, IonButton, IonInput,
+  IonItem, IonList, IonCard, IonCardContent, IonRow } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [IonRow, IonCol, IonCardContent, IonCard, IonList, IonItem,
+  IonInput, IonButton, IonContent, IonTitle, IonToolbar, CommonModule,
+  FormsModule]
+})
+export class LoginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/register/register.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonCard, IonCardContent, IonItem, IonInput, IonRow, IonCol, IonButton } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+  standalone: true,
+  imports: [IonButton, IonCol, IonRow, IonInput, IonItem, IonCardContent, IonCard, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class RegisterPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
