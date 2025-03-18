c8903f040984315549182b47cdc6580e10cd454d
Uris_8 <utti232001@utvco.edu.mx>
2025-03-17 22:28:58 -0600

    Diseño
    este es una prueba
    
    Co-Authored-By: MDZ <175367353+MDZ2999@users.noreply.github.com>

--- src/app/pages/publish-detail/publish-detail.page.ts
+++ src/app/pages/publish-detail/publish-detail.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonTextarea, IonItem, IonLabel, IonButton, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonTextarea, IonItem, IonLabel, IonButton, IonSelect, IonSelectOption, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-publish-detail',
   templateUrl: './publish-detail.page.html',
   styleUrls: ['./publish-detail.page.scss'],
   standalone: true,
-  imports: [IonButton, IonLabel, IonItem, IonTextarea, IonContent, IonHeader, IonTitle, IonToolbar, IonSelect, IonSelectOption, CommonModule, FormsModule]
+  imports: [IonInput, IonButton, IonLabel, IonItem, IonTextarea, IonContent, IonHeader, IonTitle, IonToolbar, IonSelect, IonSelectOption, CommonModule, FormsModule]
 })
 export class PublishDetailPage implements OnInit {
 
--- src/app/pages/publish/publish.page.ts
+++ src/app/pages/publish/publish.page.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonItem, IonLabel, IonTextarea, IonInput, IonRow, IonGrid, IonCol, IonList, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonItem, IonLabel, IonTextarea, IonInput, IonRow, IonGrid, IonCol, IonList, IonSelect, IonSelectOption, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { cloudUploadOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-publish',
   templateUrl: './publish.page.html',
   styleUrls: ['./publish.page.scss'],
   standalone: true,
-  imports: [IonList, IonCol, IonGrid, IonRow, IonInput, IonTextarea, IonLabel, IonItem, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonSelect, IonSelectOption]
+  imports: [IonIcon, IonList, IonCol, IonGrid, IonRow, IonInput, IonTextarea, IonLabel, IonItem, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonSelect, IonSelectOption]
 })
 export class PublishPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({ cloudUploadOutline });
+  }
 
   ngOnInit() {
   }
